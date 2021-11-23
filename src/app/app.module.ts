e71bd2148794d45df473192e4f2c6b4e7c1d8fc2
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-11-23 16:00:59 +0100

    :lipstick: : update cards style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { filter, from, map } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { NavComponent } from './layout/nav/nav.component';
 import { UnicornCardComponent } from './pages/unicorn-list/unicorn-card/unicorn-card.component';
 import { UnicornListComponent } from './pages/unicorn-list/unicorn-list.component';
 
@@ -15,33 +23,21 @@ interface Facture {
 }
 
 @NgModule({
-  declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, HeaderComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor() {
-    const facturesHT: Facture[] = [
-      { customer: 'bob', amont: 10, payed: true },
-      { customer: 'dylan', amont: 20, payed: false },
-    ];
-
-    from(facturesHT)
-      .pipe(
-        filter((facture: Facture): boolean => !facture.payed),
-        map((facture: Facture): Facture => ({ ...facture, amont: facture.amont * 1.2 }))
-      )
-      .subscribe((e) => {
-        // ICI
-      });
-
-    fetch('http://localhost:3000/unicorns')
-      .then((response) => response.json())
-      .then((unicorns) => {
-        debugger;
-      });
-
-    debugger;
-  }
-}
+export class AppModule {}
